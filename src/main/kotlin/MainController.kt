c33e2c3956d229fd32af22379c924eac6ff41ec5
Saki <10335149+Saki-EU@users.noreply.github.com>
2019-10-20 16:18:39 +0200

    Update MainController.kt

--- src/main/kotlin/MainController.kt
+++ src/main/kotlin/MainController.kt
@@ -132,7 +132,7 @@ class MainController : Initializable {
     @FXML
     private lateinit var downloaderPane: TitledPane
 
-    private val version = "6.7.2"
+    private val version = "6.7.3"
     private val command = Command()
     private var image: File? = null
     private var romDirectory: File? = null
